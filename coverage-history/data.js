5bd23991c1d07e171b6d37cb424e4675f8d46d15
github-action-benchmark <github@users.noreply.github.com>
2026-09-16 07:29:22 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for c9f2f756824c3fac008d6dc008e8d3601f217031

--- coverage-history/data.js
+++ coverage-history/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789486395072,
+  "lastUpdate": 1789543762834,
   "repoUrl": "https://github.com/bernedom/450-tictactest-mvk",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/bernedom/450-tictactest-mvk/commit/c9f2f756824c3fac008d6dc008e8d3601f217031"
         },
         "date": 1789486393455,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Instruction coverage",
+            "value": 53.333333333333336,
+            "unit": "%"
+          },
+          {
+            "name": "Branch coverage",
+            "value": 44.871794871794876,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dominik.berner@gmail.com",
+            "name": "Dominik Berner",
+            "username": "bernedom"
+          },
+          "committer": {
+            "email": "dominik.berner@gmail.com",
+            "name": "Dominik Berner",
+            "username": "bernedom"
+          },
+          "distinct": false,
+          "id": "c9f2f756824c3fac008d6dc008e8d3601f217031",
+          "message": "First try",
+          "timestamp": "2026-09-15T17:26:49+02:00",
+          "tree_id": "b774681d96dc33724a7ea248a5c7e2b4f104589d",
+          "url": "https://github.com/bernedom/450-tictactest-mvk/commit/c9f2f756824c3fac008d6dc008e8d3601f217031"
+        },
+        "date": 1789543762356,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
